7b5b9c7063ce1719b172103c549f26410da1f094
andrewthenedi <44109741+andrewthenedi@users.noreply.github.com>
2026-04-14 21:08:21 +0800

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # T: O(N) | S: O(N)
+        need_to_idx = {}
+        for i, num in enumerate(nums):
+            if num in need_to_idx:
+                return [need_to_idx[num], i]
+            need_to_idx[target - num] = i
