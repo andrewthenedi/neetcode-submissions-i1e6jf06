1f02268036a80efb0a584ab5baffedde65ae63e0
andrewthenedi <44109741+andrewthenedi@users.noreply.github.com>
2026-04-23 21:57:54 +0800

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def recursive(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # T: O(N) | S: O(H)
+        # H = O(LOG N) / O(N)
+        if not(p or q):
+            return True
+        if not(p and q) or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return self.recursive(p, q)
