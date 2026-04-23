ca5a74236488e3f90e88a9eaf8ebc3221cef68f3
andrewthenedi <44109741+andrewthenedi@users.noreply.github.com>
2026-04-23 22:16:14 +0800

    Add: same-binary-tree - submission-8

---
+++ Data Structures & Algorithms/same-binary-tree/submission-8.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def iterative(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # T: O(N) | S: O(W)
+        # W = O(N)
+        if not(p or q):
+            return True
+        if not(p and q):
+            return False
+        queue = collections.deque([(p, q)])
+        while queue:
+            p, q = queue.popleft()
+            if p.val != q.val:
+                return False
+            
+            if not(p.left or q.left):
+                pass
+            elif not(p.left and q.left):
+                return False
+            else:
+                queue.append((p.left, q.left))
+            
+            if not(p.right or q.right):
+                pass
+            elif not(p.right and q.right):
+                return False
+            else:
+                queue.append((p.right, q.right))
+        return True
+
+    def recursive(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # T: O(N) | S: O(H)
+        # H = O(LOG N) / O(N)
+        if not(p or q):
+            return True
+        if not(p and q) or p.val != q.val:
+            return False
+        return self.recursive(p.left, q.left) and self.recursive(p.right, q.right)
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return self.iterative(p, q)
+        # return self.recursive(p, q)
